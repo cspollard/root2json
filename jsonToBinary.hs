1fee9ca9f00a42a36cec499b6ee31942b4000a72
Chris Pollard <cspollard@gmail.com>
2015-11-29 15:09:41 +0000

    cleaning up.

--- jsonToBinary.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import qualified Data.ByteString.Lazy as BSL
-
-import Data.Atlas.Stream
-import Data.Atlas.TopTree
-
-main :: IO ()
-main = BSL.putStr . encodeList . parseTree =<< BSL.getContents
